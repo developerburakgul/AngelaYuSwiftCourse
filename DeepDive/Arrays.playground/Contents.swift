e9728ca5eeb72fdba67ea157655c4719504313f0
Burak Gül <developerburakgul@gmail.com>
2023-06-04 16:25:07 +0300

    DeepDive file added

---
+++ DeepDive/Arrays.playground/Contents.swift
@@ -0,0 +1,2 @@
+var numbers = [17,5,2022,9,8,2001,2,1]
+print(numbers[1])
---
+++ DeepDive/ConstantAndBasicDataTypes.playground/Contents.swift
@@ -0,0 +1,13 @@
+let isim = "Burak";
+var isim2 = "Batuhan"
+
+var sayi = 2 // int
+var sayi2 = 2.3 // float
+var sayi3 = 3.14159 // double
+var isActive = true // boolean
+
+
+Int.random(in: 0...5)// 0 ve 5 kapalı aralık
+Int.random(in: 0..<5)// o kapalı 5 açık aralık
+//array.randomElement()// rastgele bir eleman verir diziden
+//array.shuffle()// dizinin elemanlarının yerini rasgtele değiştirir
