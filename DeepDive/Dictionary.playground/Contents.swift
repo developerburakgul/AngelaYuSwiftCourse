0c56c637aab63d354390110d5ebf7a7a3f6fb1a5
Burak Gül <developerburakgul@gmail.com>
2023-06-07 18:11:41 +0300

    Optionals and Dictionaries added to DeepDive

---
+++ DeepDive/Dictionary.playground/Contents.swift
@@ -0,0 +1,9 @@
+// dictionary tanımlama
+
+var dict :[String:Int] = ["Batuhan":19,"Burak":22]
+
+// dictionary den eleman çekme
+
+var yas = dict["Burak"]
+
+print(yas!)
---
+++ DeepDive/Optionals.playground/Contents.swift
@@ -0,0 +1,11 @@
+//var username : String = nil
+
+var username : String? = nil
+
+username = "iamburakgul"
+
+//username = nil
+
+if username != nil {
+    print(username!)
+}
