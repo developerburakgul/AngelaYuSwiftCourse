36f5cb2df2083b16a1874acff37de3bfb2ca5791
Burak Gül <developerburakgul@gmail.com>
2023-06-07 16:19:49 +0300

    FunctionsAndScopes added to DeepDive

---
+++ DeepDive/FunctionsAndScope.playground/Contents.swift
@@ -0,0 +1,18 @@
+var myAge : Int = 20
+// verinin tipi bu şekilde belirlenir
+
+
+
+
+func greeting()  {
+    print("Hello")
+}
+
+greeting()
+
+func greeting2(whoToGreet  : String)  {
+    print("Hello \(whoToGreet)")
+}
+
+greeting2(whoToGreet: "Burak")
+
