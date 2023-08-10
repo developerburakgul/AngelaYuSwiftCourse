bd601c8bf7e0bf990e16c7ebb0984a9674e838ed
Burak Gül <developerburakgul@gmail.com>
2023-08-10 16:56:34 +0300

    closures playground is updated

--- DeepDive/Closures.playground/Contents.swift
+++ DeepDive/Closures.playground/Contents.swift
@@ -1,11 +1,11 @@
 import UIKit
 // Note : every episode is same
 
-let array = [6,2,3,9,4,1]
-
-func addOne(n1:Int) -> Int {
-    return n1+1
-}
+//let array = [6,2,3,9,4,1]
+//
+//func addOne(n1:Int) -> Int {
+//    return n1+1
+//}
 
 //array.map(addOne)
 
