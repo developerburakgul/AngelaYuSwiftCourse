c414912a52f729a0592ac22f93630be1ec296523
Burak Gül <developerburakgul@gmail.com>
2023-08-18 19:26:22 +0300

    Flash Chat added and Type animation is done

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: charIndex * 0.1, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex = charIndex + 1
+        }
+
+       
+    }
+    
+
+}
