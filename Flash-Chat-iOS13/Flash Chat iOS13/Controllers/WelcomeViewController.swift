86f66f27be2f8f5bd9f1a81e6efb952e222ecd85
Burak Gül <developerburakgul@gmail.com>
2023-08-19 18:57:02 +0300

    I am going on this project

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,22 +8,24 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
+        titleLabel.text = "⚡️FlashChat"
         
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: charIndex * 0.1, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex = charIndex + 1
-        }
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: charIndex * 0.1, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex = charIndex + 1
+//        }
 
        
     }
